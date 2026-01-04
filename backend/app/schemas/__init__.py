2c26701e1d63aed11fe0ecbadda7f3bca70a7fe6
mohammadimran112439 <mohammadimran112439@gmail.com>
2026-01-05 01:53:13 +0530

    department

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from .employee import Employee, EmployeeCreate, EmployeeUpdate
+from .employee import Employee, EmployeeCreate, EmployeeUpdate, EmployeeList
 from .department import Department,DepartmentCreate,DepartmentUpdate,DepartmentList
 
-__all__ = ["Employee", "EmployeeCreate", "EmployeeUpdate","Department","DepartmentCreate","DepartmentUpdate","DepartmentList",]
+__all__ = ["Employee", "EmployeeCreate", "EmployeeUpdate" ,"EmployeeList","Department","DepartmentCreate","DepartmentUpdate","DepartmentList",]
