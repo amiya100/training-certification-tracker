571991d3a161d69a447e4fea6ba0e47d6d31e132
Amiya Shabana <amiyashabana8@gmail.com>
2026-01-17 20:30:48 +0530

    viteconfig edit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-    },
+    base: "/", // <--- important for correct asset paths on Netlify
     build: {
         outDir: "dist",
         sourcemap: true,
+    },
+    server: {
+        port: 3000, // only for local dev
     },
 });
